6f36b931b4041baf7437c9dfe50575093390fefc
Devidxyz <devid@agora.xyz>
2023-07-13 22:28:00 +0200

    Export FlowMonitor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { default as Flow } from "./base/Flow";
 export { default as Queue } from "./base/Queue";
 export { default as Worker } from "./base/Worker";
+export { default as FlowMonitor } from "./base/FlowMonitor";
 export * from "./base/types";
 
 export { default as createAccessFlow } from "./access/getAccessFlow";
